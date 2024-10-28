533f201cd309c95b60339f2c432f0c42e5d63559
nuisBelon <s3448475@gmail.com>
2024-10-28 19:23:10 +0800

    解决了添加设备后程序崩溃的bug，更改了函数调用名称的规则

--- demo/VideoPreview/deviceTree.py
+++ demo/VideoPreview/deviceTree.py
@@ -1,9 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QMenu, QAction, QMessageBox, QInputDialog
 from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QFormLayout, QLabel, QLineEdit, QPushButton
 from qtpy import QtWidgets
-
+from demo.VideoPreview.AddDeviceDialog import AddDeviceDialog
 class DeviceTree(QTreeWidget):
     def __init__(self, parent=None):
         super(DeviceTree, self).__init__(parent)
@@ -39,26 +38,28 @@ class DeviceTree(QTreeWidget):
             # 显示菜单
         menu.exec_(self.viewport().mapToGlobal(QPoint(position)))
 
-    def on_add_device(self):
-        # 提示用户输入设备名称
-        text, ok = QInputDialog.getText(self, '添加设备', '请输入设备名称:')
-
-        if ok and text:
-            # 添加新设备到设备树（默认添加到根节点）
-            self.add_device(text)
-
     # def on_add_device(self):
-    #     # 创建并显示添加设备的对话框
-    #     dialog = AddDeviceDialog(self)
-    #     if dialog.exec_() == QDialog.Accepted:
-    #         # 用户点击了“添加”按钮，从对话框中获取设备信息
-    #         device_info = dialog.get_device_info()
-    #         if device_info:
-    #             # 提取设备名称和其他信息
-    #             device_name = device_info['name']
+    #     # 提示用户输入设备名称
+    #     text, ok = QInputDialog.getText(self, '添加设备', '请输入设备名称:')
     #
-    #             # 添加新设备到设备树（默认添加到根节点）
-    #             self.add_device(device_name)
+    #     if ok and text:
+    #         # 添加新设备到设备树（默认添加到根节点）
+    #         self.add_device(text)
+
+    def on_add_device(self):
+        # 创建并显示添加设备的对话框
+        dialog = AddDeviceDialog(self)
+        if dialog.exec_() == QtWidgets.QDialog.Accepted:
+            # 用户点击了“添加”按钮，从对话框中获取设备信息
+            device_info = dialog.get_device_info()
+            if device_info:
+                # 提取设备名称和其他信息
+                device_name = device_info['name']
+
+                # 添加新设备到设备树（默认添加到根节点）
+                self.add_device(device_name)
+
+
 
     def on_delete_device(self, item):
         # 确认删除
@@ -72,66 +73,6 @@ class DeviceTree(QTreeWidget):
             parent.takeChild(parent.indexOfChild(item))
 
 
-class AddDeviceDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle('添加设备')
-
-        # 创建布局和表单元素
-        self.layout = QVBoxLayout()
-
-        self.form_layout = QFormLayout()
-
-        self.name_label = QLabel('设备名称:')
-        self.name_input = QLineEdit()
-        self.form_layout.addRow(self.name_label, self.name_input)
-
-        self.address_label = QLabel('设备地址:')
-        self.address_input = QLineEdit()
-        self.form_layout.addRow(self.address_label, self.address_input)
-
-        self.port_label = QLabel('端口号:')
-        self.port_input = QIntSpinBox()
-        self.port_input.setRange(1, 65535)  # 设置合理的端口号范围
-        self.form_layout.addRow(self.port_label, self.port_input)
-
-        self.username_label = QLabel('用户名:')
-        self.username_input = QLineEdit()
-        self.form_layout.addRow(self.username_label, self.username_input)
-
-        self.password_label = QLabel('密码:')
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.Password)  # 设置为密码输入模式
-        self.form_layout.addRow(self.password_label, self.password_input)
-
-        # 添加表单布局到主布局
-        self.layout.addLayout(self.form_layout)
-
-        # 创建按钮并添加到布局
-        self.button_box = QVBoxLayout()
-        self.add_button = QPushButton('添加')
-        self.add_button.clicked.connect(self.accept)
-        self.cancel_button = QPushButton('取消')
-        self.cancel_button.clicked.connect(self.reject)
-        self.button_box.addWidget(self.add_button)
-        self.button_box.addWidget(self.cancel_button)
-        self.layout.addLayout(self.button_box)
-
-        # 设置对话框的主布局
-        self.setLayout(self.layout)
-
-    def get_device_info(self):
-        # 如果用户点击了“添加”按钮，则返回设备信息
-        if self.result() == QDialog.Accepted:
-            return {
-                'name': self.name_input.text(),
-                'address': self.address_input.text(),
-                'port': self.port_input.value(),
-                'username': self.username_input.text(),
-                'password': self.password_input.text()
-            }
-            # 如果用户点击了“取消”按钮或关闭了对话框，则返回None
-        return None
 
 ###########################################################################
 
